21295ec9bb6759d181aa97083519b365b153c074
MrHyman <mr.hyman63@gmail.com>
2018-04-12 07:53:24 +0400

    xpath try

--- src/test/java/invalidDriverFlowTest.java
+++ src/test/java/invalidDriverFlowTest.java
@@ -1,2 +1,22 @@
-public class invalidDriverFlowTest {
+import com.codeborne.selenide.Condition;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class invalidDriverFlowTest extends driverFlowTest{
+    @Test
+    public void controlsAreShownTest() {
+        $("#email").shouldBe(Condition.visible);
+
+    }
+
+//    @Test
+//    public void passwordControlIsShownTest() {
+//        driver.find_elements_by_xpath("//*[contains(text(), 'My Button')]")
+//    }
 }
